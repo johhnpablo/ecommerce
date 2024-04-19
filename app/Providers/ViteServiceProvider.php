168af15ebf37aba12d7cc85636c45ecc6fceb62c
John Pablo <92765140+johhnpablo@users.noreply.github.com>
2024-04-19 07:49:32 -0300

    creates a vite provider to create macros making it easier to import images

---
+++ app/Providers/ViteServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ViteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Vite::macro('images', fn ($asset) => $this->asset("resources/images/{$asset}"));
+    }
+}
