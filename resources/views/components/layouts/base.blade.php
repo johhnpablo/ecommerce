dd00c30863d2be06ed89dbac2504ffe8df09a8a2
John Pablo <92765140+johhnpablo@users.noreply.github.com>
2024-04-20 07:58:04 -0300

    install alpine js

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -5,7 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <title>{{ $title ?? "MLA" }}</title>
 
@@ -16,7 +19,7 @@
     @else
         {{ $navigation }}
     @endif
-    <div class="min-h-full">
+    <div class="w-full flex items-center">
         <div class="py-10">
             <main>
                 {{ $slot }}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,38 @@ export default {
         "./resources/**/*.vue",
     ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            blue: {
+                base: '#7988FD',
+                100: '#E4E8FE',
+                200: '#B1BFE4',
+                300: '#C9D1FE',
+                400: '#9AA6FD',
+                500: '#7988FD',
+                600: '#5865D9',
+                700: '#3C47B6',
+                800: '#262F92',
+                900: '#171D79',
+            },
+        },
+        fontSize: {
+                "title-1": ['68px', '74px'],
+                "title-2": ['48px', '62px'],
+                "title-3": ['36px', '46.8px'],
+                "title-4": ['28px', '36.4px'],
+                "title-5": ['16px', '20.8px'],
+        },
+        boxShadow: {
+            hard: '0px 2px 4px rgba(0, 0, 0, 0.16)',
+            regular: '0px 4px 23px rgba(0, 0, 0, 0.12)',
+            medium: '0px 4px 22px rgba(0, 0, 0, 0.06)',
+            big: '1px 5px 22px 4px rgba(0, 0, 0, 0.15)',
+            card: '0px 7px 20px rgba(0, 0, 0, 0.08)',
+            badge: '0px 2px 8px rgba(0, 0, 0, 0.16) , 0px 20px 32px rgba(0, 0, 0, 0.24)',
+
+        }
+    },
   },
   plugins: [],
 }
